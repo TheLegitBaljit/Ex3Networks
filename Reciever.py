a2f0cca9a2650ced7cfbad7b69d99c46fdb71510
TheLegitBaljit <85311237+TheLegitBaljit@users.noreply.github.com>
2023-05-12 13:04:27 +0300

    Add files via upload

--- Reciever.py
+++ Reciever.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import os
 
 ID1 = 7421
 ID2 = 5669
@@ -7,57 +8,56 @@ ID2 = 5669
 time_pt1 = []
 time_pt2 = []
 
+file_size = os.path.getsize("text.txt")
+part_size = file_size // 2 if file_size % 2 == 0 else file_size // 2 + 1
+
 
 def receive_file(filename, host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((host, port))
         s.listen()
+        conn, addr = s.accept()
         while True:
-            conn, addr = s.accept()
             with open(filename, 'wb') as f:
                 conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, "cubic".encode())
                 # Receive first half of file
                 start_time = time.time()
-                data = ""
-                while True:
-                    chunk = conn.recv(1000000).decode()
-                    if len(chunk) <= 0:
+                data = b""
+                while len(data.decode()) < part_size - 4:
+                    chunk = conn.recv(2048)
+                    if not chunk:
                         break
                     data += chunk
-                f.write(data.encode())
+                f.write(data)
                 end_time = time.time()
                 print(f'Received first half in {end_time - start_time} seconds')
                 time_pt1.append(end_time - start_time)
 
-                conn, addr = s.accept()
-
                 # Send authentication:
-                conn.sendall(str(ID1 ^ ID2).encode())
-
-                conn.close()
-
-                conn, addr = s.accept()
+                auth_msg = str(ID1 ^ ID2).encode()
+                conn.sendall(auth_msg)
 
                 # Receive second half of file
                 conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, "reno".encode())
                 start_time = time.time()
-                data = ""
-                while True:
-                    chunk = conn.recv(1000000).decode()
-                    if len(chunk) <= 0:
+                data = b""
+                while len(data.decode()) < part_size - 4:
+                    chunk = conn.recv(2048)
+                    if not chunk:
                         break
                     data += chunk
-                f.write(data.encode())
+                f.write(data)
                 end_time = time.time()
                 print(f'Received second half in {end_time - start_time} seconds')
                 time_pt2.append(end_time - start_time)
 
-                conn, addr = s.accept()
-                is_done = conn.recv(50).decode()
+                conn.sendall("again".encode())
+
+                is_done = conn.recv(18).decode()
                 if is_done == "_done_sending_all_":
-                    break
-                else:
                     conn.close()
+                    break
 
     print(time_pt1)
     print("Avg time for part 1 is: ", sum(time_pt1) / len(time_pt1))
@@ -65,4 +65,4 @@ def receive_file(filename, host, port):
     print("Avg time for part 2 is: ", sum(time_pt2) / len(time_pt2))
 
 
-receive_file('received.txt', 'localhost', 8000)
+receive_file('received.txt', 'localhost', 9000)
